5b888aed589cb82e457d47bdc4264c77ae6050bb
arturkhmeliarskiy <67556260+arturkhmeliarskiy@users.noreply.github.com>
2020-10-25 22:35:06 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,31 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let result = "";
+    while(expr.length > = > 10) {
+        let partString = expr.slice(0, 10);
+        let keyObject = "";
+        for (let i = 0; i <= 8; i += 2  ){            
+            if (partString[i] === "*"){
+                keyObject += " ";
+                i = 9;
+            } else if (partString[i] === "0"){
+                keyObject += "";
+            } else if (partString[i] === "1" && partString[i + 1] === "0"){
+                keyObject += "."
+            } else if (partString[i] === "1" && partString[i + 1] === "1"){
+                keyObject += "-"
+            }
+            }
+            if (keyObject !== " "){
+                result += MORSE_TABLE[keyObject];
+            } else {
+                result += " ";
+            }
+            expr = expr.slice(10);
+        }
+        return result;
+    }
 }
 
 module.exports = {
